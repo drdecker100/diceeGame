ee1a101d04448a34334970c9b835d018edec0c21
rotimi <dd427118@ohio.edu>
2021-12-28 17:01:26 -0500

    Update Javascript file

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-// generate random number between 1 and 6 
-randomNumber1 = Math.floor((Math.random(0)*6)+1)
+// 1-6
+var randomNumber1 = Math.floor(Math.random() * 6) +1;    
 
-// Use the random number you created in the last step to pick out a random dice image between dice1.png to dice 6.png 
+// dice1.png - dice6.png
+// var img1Path = "images/dice" + randomNumber1 + ".png"
 
-// then place this image inside the left <img> element.
+var randomDiceImage = "dice" + randomNumber1 + ".png"
 
-// format for path to image images.dice<number>.ping
-img1Path = "images/dice" + randomNumber1 + ".png"
+var randomDiceSource = "images/" + randomDiceImage
 
 //query for img tag and set attribute src witha new value for the left side
-document.querySelectorAll("img")[0].setAttribute("src",img1Path);
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceSource);
 
-randomNumber2 = Math.floor((Math.random(0)*6)+1)
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
 
-img2Path = "images/dice" + randomNumber2 + ".png"
+// var img2Path = "images/dice" + randomNumber2 + ".png"
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"
+
+var randomDiceSource2 = "images/" + randomDiceImage2
 
 // query for img tag and set attribute src witha new value for the right side
-document.querySelectorAll("img")[1].setAttribute("src",img2Path);
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceSource2);
 
 // Compare random Number 1 and random number 2
 // if random number 1 is greater than random number 2, player 1 wins
@@ -24,12 +27,12 @@ document.querySelectorAll("img")[1].setAttribute("src",img2Path);
 //else draw
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Player 1 Wins!"
+    document.querySelector("h1").textContent = "🚩Player 1 Wins!"
 }
 else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").textContent = "Player 2 Wins!"
+    document.querySelector("h1").textContent = "Player 2 Wins!🚩"
 }
 else {
-    document.querySelector("h1").textContent = "Draw"
+    document.querySelector("h1").textContent = "Draw!"
 }
 
